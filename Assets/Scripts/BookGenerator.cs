e488af77fc999de001920e088ee7aa40b56051ed
Phat Thaveepholcharoen <overtone00445@gmail.com>
2017-08-20 20:52:57 +0700

    Reduce character size

--- Assets/Scripts/BookGenerator.cs
+++ Assets/Scripts/BookGenerator.cs
@@ -7,6 +7,11 @@ public class BookGenerator : MonoBehaviour {
 	public GameObject book;
 	public float spawnTime;
 
+	void Awake() {
+		QualitySettings.vSyncCount = 0;
+		Application.targetFrameRate = 60;
+	}
+
 	// Use this for initialization
 	void Start () {
 		// Call target function every 3 seconds
