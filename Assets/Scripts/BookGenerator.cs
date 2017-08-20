9ccc3f9a615dc79b1355d5aac8cf3f34a90741e9
Phat Thaveepholcharoen <overtone00445@gmail.com>
2017-08-20 20:32:13 +0700

    Generate books from the ground. Add force to the book

--- Assets/Scripts/BookGenerator.cs
+++ Assets/Scripts/BookGenerator.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class BookGenerator : MonoBehaviour {
 
 	public GameObject book;
-	public Rigidbody bookRb;
-	public float spawnTime = 3f;
-	public float thrust  = 30f;
+	public float spawnTime;
+
 	// Use this for initialization
 	void Start () {
 		// Call target function every 3 seconds
@@ -21,12 +20,6 @@ public class BookGenerator : MonoBehaviour {
 
 	void SpawnBook()
 	{
-		float xAxis = Random.Range (-2.5f, 2.5f);
-		float zAxis = Random.Range (-2.5f, -3.7f);
-
-		// Set a new random position to the book object
-		book.gameObject.transform.position = new Vector3(xAxis, 0.5f, zAxis);
-		this.bookRb.AddForce (this.thrust, this.thrust, 0, ForceMode.Impulse);
 		Instantiate(book); // Clone a new book object
 	}
 
--- Assets/Scripts/BookMotion.cs
+++ Assets/Scripts/BookMotion.cs
@@ -5,10 +5,35 @@ using UnityEngine;
 public class BookMotion : MonoBehaviour {
 	
 	private bool isRotate;
+	private Rigidbody bookRb;
+
+	public float thrust; 
 
 	// Use this for initialization	
 	void Start () {
 		this.isRotate = true;
+		this.bookRb = GetComponent<Rigidbody> ();
+
+		int side = Random.Range (1, 3);
+		float zAxis = Random.Range (-2.5f, -3.7f);
+
+		if (side == 1) {
+			float xAxis = Random.Range (-2.5f, 0f);
+
+			// Set a new random position to the book object
+			this.transform.position = new Vector3(xAxis, 1f, zAxis);
+			this.bookRb.AddForce (this.thrust / 3, this.thrust, 0, ForceMode.Acceleration);
+
+		} else if (side == 2) {
+			float xAxis = Random.Range (0f, 2.5f);
+			// Set a new random position to the book object
+			this.transform.position = new Vector3(xAxis, 1f, zAxis);
+			this.bookRb.AddForce (-this.thrust / 3, this.thrust, 0, ForceMode.Acceleration);
+		}
+
+
+	
+
 	}
 	
 	// Update is called once per frame
@@ -20,12 +45,16 @@ public class BookMotion : MonoBehaviour {
 		}
 	}
 
+	void FixedUpdate() {
+		
+	}
+
 	void OnCollisionEnter(Collision col)
 	{	
 		if (col.gameObject.name == "Floor")
 		{
 			this.isRotate = false;
-			Invoke ("RemoveBook", 2f); // Call RemoveBook funtion after 2 seconds
+			Invoke ("RemoveBook", 4f); // Call RemoveBook funtion after 2 seconds
 		}
 	}
 
