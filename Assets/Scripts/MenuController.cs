d1dcf341e721995a9aafa19262a12cedb27a58c3
Phat Thaveepholcharoen <overtone00445@gmail.com>
2017-09-01 18:47:09 +0700

    Add start menu scene. Add new GUI assets

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+	public string[] gameLevel = { "easy", "medium", "hard" };
+	public string currentGameMode;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void setGameMode(string mode){
+		this.currentGameMode = mode;
+	}
+		
+}
