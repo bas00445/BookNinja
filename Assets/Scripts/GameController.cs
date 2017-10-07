4ff69ff6ffa0f094a92994b99b3787cba99cbb78
Phat Thaveepholcharoen <overtone00445@gmail.com>
2017-10-07 15:04:33 +0700

    New main menu design.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour {
 
 	public Text scoreText;
-    public Text fpsText;
 	public int score; // Global score to display
     public float deltaTime;
 
@@ -15,12 +14,14 @@ public class GameController : MonoBehaviour {
     // Use this for initialization
     void Start () {
 		this.score = 0;
-		this.scoreText.text = "Score: " + this.score.ToString ();
+        this.scoreText.text = "" ;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.scoreText.text = "Score: " + this.score.ToString ();
-
+        if (this.isGameStart)
+        {
+            this.scoreText.text = "Score: " + this.score.ToString();
+        }
     }
 }
