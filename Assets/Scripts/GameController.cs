30c768ca7b4affba21be397ffb48116e164bf893
Phat Thaveepholcharoen <overtone00445@gmail.com>
2017-10-26 18:49:48 +0700

    Add timeLeft Text to the GUI

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,24 +7,33 @@ public class GameController : MonoBehaviour {
 
 	public Text scoreText;
     public Text comboText;
+    public Text timeLeftText;
 	public int score; // Global score to display
-    public int combo = 1;
-    public float deltaTime;
+    public int combo;
+    public float timeLeft;
 
     public bool isGameStart = false;
 
     // Use this for initialization
     void Start () {
 		this.score = 0;
-        this.scoreText.text = "" ;
+        this.combo = 1;
+        this.timeLeft = 90;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (this.isGameStart)
         {
+            this.timeLeft -= Time.deltaTime;
+            this.timeLeftText.text = "Time Left: " + ((int)(this.timeLeft)).ToString();
             this.scoreText.text = "Score: " + this.score.ToString();
             this.comboText.text = "x" + this.combo.ToString();
+
+            if(this.timeLeft < 0)
+            {
+                Debug.Log("Game Over");
+            }
         }
     }
 }
