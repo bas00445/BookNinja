b700571fb26b3e778f23a4b4b5067a0bf1f25438
Phat Thaveepholcharoen <overtone00445@gmail.com>
2017-10-28 15:01:08 +0700

    Combo bar animation

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,6 +9,7 @@ public class GameController : MonoBehaviour {
     public Text comboText;
     public Text timeLeftText;
     public GameObject comboBar;
+    private Animator comboBarAnim;
 
     public MenuController menuController;
 
@@ -23,11 +24,13 @@ public class GameController : MonoBehaviour {
     void Start () {
 		this.score = 0;
         this.combo = 1;
-        this.timeLeft = 10;
+        this.timeLeft = 15;
         this.scoreText.text = "";
         this.comboText.text = "";
         this.timeLeftText.text = "";
-	}
+        this.comboBarAnim = this.comboBar.GetComponent<Animator>();
+        this.comboBarAnim.SetTrigger("isReduceCombo");
+    }
 
     void GameOver() {
         this.isGameStart = false;
@@ -40,6 +43,9 @@ public class GameController : MonoBehaviour {
 
             if (this.timeLeft >= 0)
             {
+                if(Input.GetMouseButtonDown(0)) { 
+                    this.comboBarAnim.SetTrigger("isNewCombo");
+                }
                 this.timeLeft -= Time.deltaTime;
                 this.timeLeftText.text = "Time Left: " + ((int)(this.timeLeft)).ToString();
                 this.scoreText.text = "Score: " + this.score.ToString();
